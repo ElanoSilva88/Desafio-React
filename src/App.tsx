8a752eaf0a43d0e2b4ac55526d1642f4b6b0c0fb
Elano Silva <elano_16@yahoo.com.br>
2024-05-02 09:52:24 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [list, setList] = useState([])
+
+  const handleClick = (event) => {
+
+    const newDot = {
+      clientX: event.clientX,
+      clientY: event.clientY,
+    }
+    console.log(newDot)
+
+    setList((prev) => [...prev, newDot]);
+
+  }
+
+
+  return (
+  
+  <div id="page" onClick={handleClick}>
+    {list.map((item) => (
+    <span 
+    className='dot'
+    style={{ left: item.clientX, top: item.clientY}} /> 
+   ))};
+  </div>
+  );
+}
+export default App
